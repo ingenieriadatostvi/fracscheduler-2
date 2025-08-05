78297735a08f190517ac1c463e49610f8bd874f5
rodriesquivel13 <rodrigoesquivelcastillo@gmail.com>
2025-08-05 13:28:32 -0700

    style: adding tooltips for issues in wishfuldates

--- controllers/fraction_hunter.py
+++ controllers/fraction_hunter.py
@@ -1,5 +1,5 @@
 import calendar
-from datetime import datetime
+from datetime import datetime, date
 
 from flask import render_template, request
 
@@ -15,46 +15,55 @@ from utils.hollydays import regular_hollydays_dic, snow_hollydays_dic
 
 @controllers.route('/hunt_fraction')
 def hunt_fraction():
-    date_str = request.args.get('hunter_date')
+    date_str  = request.args.get('hunter_date')
     apartment = request.args.get('apartament', 204, type=int)
 
-    if not date_str:
-        return "No date provided", 400
+    # Inicializar variables
+    error_message       = None
+    selected_fractions  = []
 
+    # Validar entrada de fecha
+    if date_str:
+        try:
+            wish = datetime.strptime(date_str, "%Y-%m-%d").date()
+        except ValueError:
+            error_message = "Formato de fecha inválido"
+            wish          = date.today()
+    else:
+        error_message = "No se proporcionó ninguna fecha"
+        wish          = date.today()
+
+    # Parámetros del apartamento
     maintenance_path = apartament_maintenance_path.get(apartment, 1)
-    weekday_start = apartament_weekday_calendar_starts.get(apartment, 1)
-    _, _, hunter = choose_utils(apartment)
+    weekday_start    = apartament_weekday_calendar_starts.get(apartment, 1)
+    idx_maker, unfract_list, hunter = choose_utils(apartment)
 
-    try:
-        wish = datetime.strptime(date_str, "%Y-%m-%d")
-    except ValueError:
-        return "Invalid date format", 400
+    # Ejecutar hunter solo si no hubo error de parseo
+    if not error_message:
+        result = hunter(
+            wish.year, wish.month, wish.day,
+            weekday_start, maintenance_path
+        )
+        if isinstance(result, str):
+            error_message      = result
+        else:
+            selected_fractions = [result[0]]
 
-    result = hunter(
-        wish.year, wish.month, wish.day,
-        weekday_start, maintenance_path
-    )
+    # Reconstruir datos del calendario
+    apt_type = apartament_type.get(apartment, "regular")
+    months   = build_months(wish.year, apt_type)
 
-    if isinstance(result, str):
-        return result, 404
-
-    # rebuild all data for that year
-    idx_maker, unfract_list, _ = choose_utils(apartment)
-    frac_idx = idx_maker(wish.year, weekday_start, maintenance_path)
+    frac_idx  = idx_maker(wish.year,     weekday_start, maintenance_path)
     frac_prev = idx_maker(wish.year - 1, weekday_start, maintenance_path)
     frac_next = idx_maker(wish.year + 1, weekday_start, maintenance_path)
 
-    unf_curr = unfract_list(wish.year, weekday_start, maintenance_path)
+    unf_curr = unfract_list(wish.year,     weekday_start, maintenance_path)
     unf_prev = unfract_list(wish.year - 1, weekday_start, maintenance_path)
     unf_next = unfract_list(wish.year + 1, weekday_start, maintenance_path)
 
-    apt_type = apartament_type.get(apartment, "regular")
-    months = build_months(wish.year, apt_type)
-
-    display_cal = calendar.Calendar(firstweekday=0)
-    prev_dec = display_cal.monthdayscalendar(wish.year - 1, 12)
-    day_names = [calendar.day_abbr[i] for i in range(7)]
-
+    display_cal  = calendar.Calendar(firstweekday=0)
+    prev_dec     = display_cal.monthdayscalendar(wish.year - 1, 12)
+    day_names    = [calendar.day_abbr[i] for i in range(7)]
     golden_holidays = (
         snow_hollydays_dic(wish.year) if apt_type == "snow"
         else regular_hollydays_dic(wish.year)
@@ -78,6 +87,7 @@ def hunt_fraction():
         unfractional_dates=unf_curr,
         unfractional_dates_prev=unf_prev,
         unfractional_dates_next=unf_next,
-        selected_fractions=[result[0]],
+        selected_fractions=selected_fractions,
         golden_holidays=golden_holidays,
+        error_message=error_message
     )
